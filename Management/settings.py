b9e2530a5c060429ace792a2adca7bb6b0362087
Aayush-Chheda <aayush.chheda@somaiya.edu>
2021-05-07 13:24:13 +0530

    added-hotel-templates

--- Management/settings.py
+++ Management/settings.py
@@ -124,8 +124,8 @@ STATIC_URL = '/static/'
 #     os.path.join(BASE_DIR, "static")
 # ]
 
-# MEDIA_URL = '/images/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
+MEDIA_URL = ''
+MEDIA_ROOT = BASE_DIR / ''
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
