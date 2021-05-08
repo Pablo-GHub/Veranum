78bb418e5f40a755240f5acce3468299ab37e4d9
Aayush-Chheda <aayush.chheda@somaiya.edu>
2021-05-08 12:20:41 +0530

    final push

--- Home/views.py
+++ Home/views.py
@@ -5,12 +5,10 @@ from django.db.models import Count
 # Create your views here.
 def home(request):
 
-    category_list = Category.objects.all()
-    category_view = Category.objects.annotate(property_count = Count('property')).values('name', 'property_count')
-    print(category_view)
+    category_list = Category.objects.annotate(count = Count('property')).values('name', 'count', 'image')
+    print(category_list)
     template = 'home.html'
     context = { 
         'category_list' : category_list,
-        'category_view' : category_view,
     }
     return render(request, template, context)
--- Management/settings.py
+++ Management/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'About',
     'Contact',
     'Home',
+    'bootstrap4'
 ]
 
 MIDDLEWARE = [
